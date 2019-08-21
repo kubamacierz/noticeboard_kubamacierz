cfdfe93a960b7784a2966082b876da7910c582e2
kuba.macierz <kuba.macierz@gmail.com>
2019-08-21 20:45:28 +0200

    Fixed more 'categoty' to 'category' typos.

--- src/AppBundle/Form/CategoryType.php
+++ src/AppBundle/Form/CategoryType.php
@@ -13,7 +13,7 @@ class CategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('categotyName');
+        $builder->add('categoryName');
     }/**
      * {@inheritdoc}
      */
